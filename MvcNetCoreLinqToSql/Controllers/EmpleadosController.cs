5ed8f4c6ac72c1044bf956e9b3252f89709a9afa
RazvanLucacii <rlucaci48@gmail.com>
2024-02-13 09:02:54 +0100

    Add project files.

---
+++ MvcNetCoreLinqToSql/Controllers/EmpleadosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreLinqToSql.Models;
+using MvcNetCoreLinqToSql.Repositories;
+
+namespace MvcNetCoreLinqToSql.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repoEmp;
+
+        public EmpleadosController()
+        {
+            this.repoEmp = new RepositoryEmpleados();
+        }
+        public IActionResult Index()
+        {
+            List<Empleado> empleados = this.repoEmp.GetEmpleados();
+            return View(empleados);
+        }
+
+        public IActionResult Detalles(int id)
+        {
+            Empleado empleado = this.repoEmp.FindEmpleado(id);
+            return View(empleado);
+        }
+    }
+}
---
+++ MvcNetCoreLinqToSql/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,55 @@
+﻿using MvcNetCoreLinqToSql.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MvcNetCoreLinqToSql.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private DataTable tablaEmpleados;
+
+        public RepositoryEmpleados()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Password=MCSD2023";
+            string sql = "select * from EMP";
+            SqlDataAdapter adEmp = new SqlDataAdapter(connectionString, sql);
+            this.tablaEmpleados = new DataTable();
+            adEmp.Fill(tablaEmpleados);
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable() 
+                           select datos;
+            List<Empleado> empleados = new List<Empleado>();
+            foreach (var row in consulta)
+            {
+                Empleado emp = new Empleado();
+                emp.IdEmpleado = row.Field<int>("EMP_NO");
+                emp.Apellido = row.Field<string>("APELLIDO");
+                emp.Oficio = row.Field<string>("OFICIO");
+                emp.Salario = row.Field<int>("SALARIO");
+                emp.IdDepartamento = row.Field<int>("DEPT_NO");
+                empleados.Add(emp);
+            }
+            return empleados;
+        }
+
+        public Empleado FindEmpleado(int idEmpleado)
+        {
+            //alias datos representa cada linea/objeto dentro del conjunto
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable()
+                           where datos.Field<int>("EMP_NO") == idEmpleado
+                           select datos;
+            var row = consulta.First();
+            Empleado empleado = new Empleado();
+            empleado.IdEmpleado = row.Field<int>("EMP_NO");
+            empleado.Apellido = row.Field<string>("APELLIDO");
+            empleado.Oficio = row.Field<string>("OFICIO");
+            empleado.Salario = row.Field<int>("SALARIO");
+            empleado.IdDepartamento = row.Field<int>("DEPT_NO");
+            return empleado;
+
+        }
+    }
+}
